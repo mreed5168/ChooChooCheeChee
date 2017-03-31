ba180be26b1977d69b3f54eb1aaced9530f23294
Michael Reed <mreed5168@Michael-Reeds-MacBook-Pro.local>
2017-03-31 08:20:09 -0500

    logic updated

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,62 +1,61 @@
 var config = {
-    apiKey: "AIzaSyCO0C5EnPnSzPD2UT2ikjVJ2md4ulvpWGc",
-    authDomain: "timesheet-9f77c.firebaseapp.com",
-    databaseURL: "https://timesheet-9f77c.firebaseio.com",
-    storageBucket: "timesheet-9f77c.appspot.com",
-    messagingSenderId: "73161317965"
+    apiKey: "AIzaSyA6GZrgit-EACA7iug5LSQ_7Al1SsieTus",
+    authDomain: "trains-64227.firebaseapp.com",
+    databaseURL: "https://trains-64227.firebaseio.com",
+    storageBucket: "trains-64227.appspot.com",
+    messagingSenderId: "143446148056"
   };
   
   firebase.initializeApp(config);
   var database = firebase.database();
 
-
-var employeeName;
-var role;
-var startDate;
-var monthlyRate;
-var allIds = [];
-
+// Clicking submit calls getData function
 $("#submit").on("click", function(event){
 	event.preventDefault();
-console.log("goodjob");
-getUser();
+  getData();
 })
 
+// Functions ===================================
 
+// This function is called by the submit button being clicked
+// This function gathers data and pushes it to firebase
+function getData() {
+trainName = $("#trainName").val().trim();
+destination = $("#destination").val().trim();
+firstTrain = $("#firstTrain").val().trim();
+frequency = $("#frequency").val().trim();
 
-function getUser() {
-employeeName = $("#employeeName").val().trim();
-role = $("#role").val().trim();
-startDate = $("#startDate").val().trim();
-monthlyRate = $("#monthlyRate").val().trim();
-console.log(employeeName);
-console.log(role);
-console.log(startDate);
-console.log(monthlyRate);
-
-
-
-
+// Pushing info to database
 database.ref().push({
-  name : employeeName,
-  position : role,
-  start : startDate,
-  rate : monthlyRate,
-});
+  trainName : trainName,
+  destination : destination,
+  firstTrain : firstTrain,
+  frequency : frequency,
+  });
+
+// Emptying divs after clicking submit
+$("#trainName").val("");
+$("#destination").val("");
+$("#firstTrain").val("");
+$("#frequency").val("");
 
 };
 
+
+// This function notices if a child is added to the database
+// If a child is added the information is then added to the website
 database.ref().on("child_added", function(snapshot){
+var train = snapshot.val().trainName;
+var dest = snapshot.val().destination;
+var fTrain = snapshot.val().firstTrain;
+var freq = snapshot.val().frequency;
 
-console.log(snapshot.val());
+console.log(train);
+console.log(dest);
+console.log(fTrain);
+console.log(freq);
 
-console.log(snapshot.val().name);
-
-console.log(snapshot.val().position);
-
-console.log(snapshot.val().start);
-
-console.log(snapshot.val().rate);
+$("#trainTable > tbody").append("<tr><td>" + train + "</td><td>" + dest + "</td><td>" + freq + "</td><td>" + "nextA" + "</td><td>" + "minAway" + "</td></tr>");
 
 });
 
